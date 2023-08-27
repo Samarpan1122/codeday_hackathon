b1380617f7a4a7a4cc6ee8895fc9b5469f8e617e
Samarpan1122 <samarpanmohanty@gmail.com>
2023-08-27 18:52:04 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+  export const firebaseConfig = {
+    apiKey: "AIzaSyA-zwS2QZeFLkqDWOc-xR4uUawuaWtVK1s",
+    authDomain: "codeday-32a37.firebaseapp.com",
+    databaseURL: "https://codeday-32a37-default-rtdb.firebaseio.com",
+    projectId: "codeday-32a37",
+    storageBucket: "codeday-32a37.appspot.com",
+    messagingSenderId: "306519396408",
+    appId: "1:306519396408:web:3259cac90ea070238bd6cd",
+    measurementId: "G-QYL57RYSHE"
+  };
